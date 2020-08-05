509b96b67c7cdef87b7d7c219ff107b5a809da4a
Leo Nguyen <hoangsang160898@gmail.com>
2020-08-05 12:30:04 +0700

    update webpack todolist

---
+++ todolist/src/components/common/presentational/Input.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Input = styled.input`
+	width: 100%;
+	outline: none;
+	padding: 16px;
+	color: #4d4d4d;
+	border: none;
+	background: rgba(0, 0, 0, 0.003);
+	box-shadow: 9px 7px 40px -6px rgba(0, 0, 0, 0.25);
+	font-size: 16px;
+`
+
+const GeneralInput = ({
+	text,
+	placeholder,
+	onChange,
+	value,
+	onKeyPress,
+}) => (
+	<Input
+		type={text}
+		placeholder={placeholder}
+		onChange={onChange}
+		value={value}
+		onKeyPress={onKeyPress}
+	/>
+)
+
+export default GeneralInput
